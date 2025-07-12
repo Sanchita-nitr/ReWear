3583ee370fc17388e06a5de4c783ee840c81eae8
Nitish Kumar <iamnitishsah12@gmail.com>
2025-07-12 11:12:45 +0530

    Update urls.py

--- ReWear-Backend/users/urls.py
+++ ReWear-Backend/users/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('auth/send-otp/', SendOTPView.as_view(), name='send-otp'),
     path('auth/verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     path('auth/register/', UserRegisterView.as_view(), name='user-register'),
-    path('auth/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('auth/me/', UserProfileView.as_view(), name='user-profile'),
     path('auth/users/', UserListView.as_view(), name='user-list'),
 ]
